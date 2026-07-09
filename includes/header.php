e893ead9fff3e031bba7c999b80c44bfc110a1e5
PeruxD <rezakeaocrew@gmail.com>
2026-07-09 09:44:44 -0500

    Fix: Use absolute URLs with base_url for proper navigation across all pages

--- includes/header.php
+++ includes/header.php
@@ -1,29 +1,29 @@
 <?php
-include '../config/constants.php';
+$base_url = '/3/';
 ?>
 
 <header class="main-header">
     <nav class="navbar">
         <div class="navbar-container">
             <div class="logo">
-                <a href="index.php">
+                <a href="<?php echo $base_url; ?>index.php">
                     <h1>🎮 FORESTER</h1>
                 </a>
             </div>
             
             <ul class="nav-menu">
-                <li><a href="index.php">Inicio</a></li>
-                <li><a href="games.php">Juegos</a></li>
-                <li><a href="ranking.php">Ranking</a></li>
-                <li><a href="tournaments.php">Torneos</a></li>
-                <li><a href="community.php">Comunidad</a></li>
+                <li><a href="<?php echo $base_url; ?>index.php">Inicio</a></li>
+                <li><a href="<?php echo $base_url; ?>games.php">Juegos</a></li>
+                <li><a href="<?php echo $base_url; ?>ranking.php">Ranking</a></li>
+                <li><a href="<?php echo $base_url; ?>tournaments.php">Torneos</a></li>
+                <li><a href="<?php echo $base_url; ?>community.php">Comunidad</a></li>
                 
                 <?php if(isset($_SESSION['user_id'])): ?>
-                    <li><a href="profile.php">Mi Perfil</a></li>
-                    <li><a href="logout.php">Cerrar Sesión</a></li>
+                    <li><a href="<?php echo $base_url; ?>profile.php">Mi Perfil</a></li>
+                    <li><a href="<?php echo $base_url; ?>logout.php">Cerrar Sesión</a></li>
                 <?php else: ?>
-                    <li><a href="auth/login.php" class="btn-login">Iniciar Sesión</a></li>
-                    <li><a href="auth/register.php" class="btn-register">Registrarse</a></li>
+                    <li><a href="<?php echo $base_url; ?>auth/login.php" class="btn-login">Iniciar Sesión</a></li>
+                    <li><a href="<?php echo $base_url; ?>auth/register.php" class="btn-register">Registrarse</a></li>
                 <?php endif; ?>
             </ul>
             
